8bd313e8dc5e7be1743dd24306ef67bbdde3e3f8
igormassami9 <igormassami9@gmail.com>
2023-05-24 03:53:11 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private router: Router) {
+    const isLoggedIn = this.checkIfUserIsLoggedIn(); // Verifica se o usuário já está logado
+
+    if (isLoggedIn) {
+      this.router.navigateByUrl('/tabs/tab1'); // Redireciona para a página de tabs se o usuário já estiver logado
+    } else {
+      this.router.navigateByUrl('/login'); // Redireciona para a página de login se o usuário não estiver logado
+    }
+  }
+
+  checkIfUserIsLoggedIn(): boolean {
+    // Lógica para verificar se o usuário já está logado
+    // Retorne true se o usuário estiver logado, caso contrário, retorne false
+    return false; // Exemplo: nenhum usuário logado
+  }
 }
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -19,21 +19,15 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
-      {
-        path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
+      // ... outras rotas das páginas das tabs
+      { path: '', redirectTo: '/tabs/tab1', pathMatch: 'full' } // Define a rota padrão dentro das tabs
     ]
   },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+  { path: '', redirectTo: '/login', pathMatch: 'full' } // Redireciona para a página de login caso não haja nenhuma rota correspondente
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
